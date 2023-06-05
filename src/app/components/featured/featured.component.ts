74e9c059e6a271c04424f19b421f37ca4e15e154
Naoki <murilomelo.code@gmail.com>
2023-06-05 13:43:14 -0300

    update: angular 16

--- src/app/components/featured/featured.component.ts
+++ src/app/components/featured/featured.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { IShow } from 'src/app/models/show.model';
+import { ShowsService } from 'src/app/services/shows.service';
 
 @Component({
   selector: 'app-featured',
   templateUrl: './featured.component.html',
-  styleUrls: ['./featured.component.css']
+  styleUrls: ['./featured.component.css'],
 })
 export class FeaturedComponent {
-
+  movie: IShow = new ShowsService().getMovies()[1];
 }
